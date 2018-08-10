8ef7c879abeba2faf2ee689a6a33d3a8067e3e10
sergey <grishnyakov2@ya.ru>
2018-08-10 17:30:35 +0500

    bind device - autoupdate

--- config/index.js
+++ config/index.js
@@ -13,8 +13,8 @@ module.exports = {
     proxyTable: {},
 
     // Various Dev Server settings
-    urlServer: 'http://localhost:7877', //server ip
-    //urlServer: 'http://89.31.33.164:7877', //server ip
+   // urlServer: 'http://localhost:7877', //server ip
+    urlServer: 'http://89.31.33.164:7877', //server ip
     host: 'localhost', // can be overwritten by process.env.HOST
   //    host: '89.31.33.164', // can be overwritten by process.env.HOST
     port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
